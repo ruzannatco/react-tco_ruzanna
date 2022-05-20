e2a2a09a897cfbaef5db1513ce5cb2b82b0b2e95
ruzannatco <ruzanna.tco@gmail.com>
2022-05-20 14:38:09 +0400

    Fixed Select deafult value request issue

--- src/components/Project/MainSection/Head/HeadRight/SortSelect/index.jsx
+++ src/components/Project/MainSection/Head/HeadRight/SortSelect/index.jsx
@@ -3,16 +3,14 @@ import "./styles.css"
 
 export const SortSelect = ({handleSelect}) => {
     return (
-        <Input name="sort_by" type="select" className="custom-select" onChange={handleSelect} >
-            <option defaultValue="Sort By">Sort By</option>
+        <Input name="sort_by" type="select" className="custom-select" onChange={handleSelect}>
+            <option value="">Sort By</option>
             <option value="a-z">A-Z</option>
             <option value="z-a">Z-A</option>
             <option value="creation_date_oldest">Created oldest</option>
             <option value="creation_date_newest">Created newest</option>
             <option value="completion_date_oldest">Completed oldest</option>
             <option value="completion_date_newest">Completed newest</option>
-            <option>Oldest First</option>
-            <option>Todo at Newest</option>
         </Input>
     );
 }
