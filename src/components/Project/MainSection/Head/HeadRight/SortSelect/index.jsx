3ee044eb7dbdfcfa0f7e676ea6bf9d4252b18edf
ruzannatco <ruzanna.tco@gmail.com>
2022-05-23 11:04:09 +0400

    Updated Sort select data to dynamic object

--- src/components/Project/MainSection/Head/HeadRight/SortSelect/index.jsx
+++ src/components/Project/MainSection/Head/HeadRight/SortSelect/index.jsx
@@ -1,17 +1,15 @@
 import { memo } from "react";
 import {Input} from "reactstrap";
 import "./styles.css"
+import {SORT_FIELDS} from "../../../../../../const";
 
 export const SortSelect = memo(({handleSelect}) => {
     return (
         <Input name="sort_by" type="select" className="custom-select" onChange={handleSelect}>
             <option value="">Sort By</option>
-            <option value="a-z">A-Z</option>
-            <option value="z-a">Z-A</option>
-            <option value="creation_date_oldest">Created oldest</option>
-            <option value="creation_date_newest">Created newest</option>
-            <option value="completion_date_oldest">Completed oldest</option>
-            <option value="completion_date_newest">Completed newest</option>
+            {SORT_FIELDS.map(({value, label}) => {
+                return <option value={value} key={label}>{label}</option>
+            })}
         </Input>
     );
 })
